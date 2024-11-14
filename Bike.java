cf68fbcb67227a506f9125f53930f565c6dcac5c
youngscott1 <143590661+youngscott1@users.noreply.github.com>
2024-11-14 14:34:41 +0530

    Delete Bike.java

--- Bike.java
+++
@@ -1,6 +0,0 @@
-package in.ac.adit.pwj.miniproject.parking;
-public class Bike extends Vehicle {
-    public Bike(String vehicleId, String licensePlate) {
-        super(vehicleId, licensePlate, "Bike");
-    }
-}
