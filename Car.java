792a7e7b866bad8fe0a496e526c380ce1017d728
youngscott1 <143590661+youngscott1@users.noreply.github.com>
2024-11-14 14:34:29 +0530

    Delete Car.java

--- Car.java
+++
@@ -1,7 +0,0 @@
-package in.ac.adit.pwj.miniproject.parking;
-
-public class Car extends Vehicle {
-    public Car(String vehicleId, String licensePlate) {
-        super(vehicleId, licensePlate, "Car");
-    }
-}
