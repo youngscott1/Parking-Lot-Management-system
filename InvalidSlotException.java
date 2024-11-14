023dd7f88a0cace67f611458c34c156e78ff8b93
youngscott1 <143590661+youngscott1@users.noreply.github.com>
2024-11-14 14:34:51 +0530

    Delete InvalidSlotException.java

--- InvalidSlotException.java
+++
@@ -1,7 +0,0 @@
-package in.ac.adit.pwj.miniproject.parking;
-
-public class InvalidSlotException extends Exception {
-    public InvalidSlotException(String message) {
-        super(message);
-    }
-}
